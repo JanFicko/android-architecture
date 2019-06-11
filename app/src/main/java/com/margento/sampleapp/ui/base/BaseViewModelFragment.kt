070cbe635bd20de92d5890d2c677238d0fc721b1
Jan Ficko <jan.ficko@gmail.com>
2019-06-11 12:40:14 +0200

    - Code refactor

--- app/src/main/java/com/margento/sampleapp/ui/base/BaseViewModelFragment.kt
+++ app/src/main/java/com/margento/sampleapp/ui/base/BaseViewModelFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
-import kotlin.contracts.ExperimentalContracts
 
 abstract class BaseViewModelFragment<ST, VM : BaseViewModel<ST>> : Fragment() {
 
